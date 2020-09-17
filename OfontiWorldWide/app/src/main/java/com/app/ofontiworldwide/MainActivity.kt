25d5d13de6782a20b031c7f4ae874fbbbac8288c
Emuolanari <emuolao@gmail.com>
2020-09-17 20:41:03 +0100

    jsoup added to edit html

--- OfontiWorldWide/app/src/main/java/com/app/ofontiworldwide/MainActivity.kt
+++ OfontiWorldWide/app/src/main/java/com/app/ofontiworldwide/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.app.ofontiworldwide
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebSettings
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jsoup.Jsoup
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,11 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //val document = Jsoup.connect("https://www.ofonti.com").get()
+        //document.getElementsByClass("site-footer").remove()
+        //webView.loadData(document.toString(),"text/html","utf-8")
+        //webView.loadDataWithBaseURL("https://www.ofonti.com",document.toString(),"text/html","utf-8","")
+
         webView.webViewClient = WebViewClient()
         webView.loadUrl("https://www.ofonti.com")
 
         val webSettings = webView.settings
         webSettings.javaScriptEnabled = true
+
     }
 
     override fun onBackPressed() {
