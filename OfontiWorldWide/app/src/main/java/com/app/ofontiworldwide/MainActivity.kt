9fbb5fcf6d00439e275e7debf071ad9100cf6ac9
Emuolanari <emuolao@gmail.com>
2020-09-19 09:49:26 +0100

    added progressbar

--- OfontiWorldWide/app/src/main/java/com/app/ofontiworldwide/MainActivity.kt
+++ OfontiWorldWide/app/src/main/java/com/app/ofontiworldwide/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.app.ofontiworldwide
+import android.graphics.Bitmap
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jsoup.Jsoup
 
@@ -16,13 +22,35 @@ class MainActivity : AppCompatActivity() {
         //document.getElementsByClass("site-footer").remove()
         //webView.loadData(document.toString(),"text/html","utf-8")
         //webView.loadDataWithBaseURL("https://www.ofonti.com",document.toString(),"text/html","utf-8","")
+        setupWebView()
 
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://www.ofonti.com")
+    }
 
-        val webSettings = webView.settings
-        webSettings.javaScriptEnabled = true
+    private fun setupWebView() {
 
+        val webViewClient: WebViewClient = object: WebViewClient() {
+
+            @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                view?.loadUrl(request?.url.toString())
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                progressBar.visibility = View.VISIBLE
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                progressBar.visibility = View.GONE
+                super.onPageFinished(view, url)
+            }
+        }
+        webView.webViewClient = webViewClient
+        webView.loadUrl("https://www.ofonti.com/")
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.defaultTextEncodingName = "utf-8"
     }
 
     override fun onBackPressed() {
